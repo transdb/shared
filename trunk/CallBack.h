a56aac05142e03c8ec72bc7b3af0fa984fc4fe6a
wayland <wayland@89b511ab-9c3f-d142-8dbd-1a3b8e1c1a46>
2014-07-21 09:19:07 +0000

    + f

--- trunk/CallBack.h
+++ trunk/CallBack.h
@@ -33,34 +33,20 @@ private:
     DISALLOW_COPY_AND_ASSIGN(CallbackBase);
 };
 
-class CallbackFP
-{
-	typedef void(*callback)();
-	callback mycallback;
-public:
-	CallbackFP(callback cb) : mycallback(cb) {}
-	void operator()();
-	void execute();
-	void set(callback cb) { mycallback = cb; }
-	CallbackFP* Create();
-};
-
 template <class Class>
 class CallbackP0 : public CallbackBase
 {
+    typedef void (Class::*Method)();
+    
 public:
-
-	typedef void (Class::*Method)();
-	CallbackP0(Class* _class_instance, Method _method)
+	explicit CallbackP0(Class* _class_instance, Method _method) : m_obj(_class_instance), m_func(_method)
 	{
-	   m_obj = _class_instance;
-	   m_func = _method;
-	};
-	void operator()() { return (m_obj->*m_func)(); }
-	void execute() { return operator()(); }
+	}
+    
+	void operator()()   { return (m_obj->*m_func)(); }
+	void execute()      { return operator()(); }
 
-//private:
-
+private:
 	Class*  m_obj;
 	Method  m_func;
 };
@@ -68,105 +54,83 @@ public:
 template <class Class, typename P1>
 class CallbackP1 : public CallbackBase
 {
+    typedef void (Class::*Method)(P1);
+    
 public:
-
-	typedef void (Class::*Method)(P1);
-	CallbackP1(Class* _class_instance, Method _method, P1 p1)
+	explicit CallbackP1(Class* _class_instance, Method _method, P1 p1) : m_obj(_class_instance), m_func(_method), m_p1(p1)
 	{
-	   _obj = _class_instance;
-	   _func = _method;
-	   _p1 = p1;
-	};
+	}
 
-	void operator()() { return (_obj->*_func)(_p1); }
-	void execute() { return operator()(); }
+	void operator()()   { return (m_obj->*m_func)(m_p1); }
+	void execute()      { return operator()(); }
 
-//private:
-
-	Class*  _obj;
-	Method  _func;
-	P1 _p1;
+private:
+	Class*  m_obj;
+	Method  m_func;
+	P1 m_p1;
 };
 
 template <class Class, typename P1, typename P2>
 class CallbackP2 : public CallbackBase
 {
+    typedef void (Class::*Method)(P1, P2);
+    
 public:
-
-	typedef void (Class::*Method)(P1, P2);
-	CallbackP2(Class* _class_instance, Method _method, P1 p1, P2 p2)
+	explicit CallbackP2(Class* _class_instance, Method _method, P1 p1, P2 p2) : m_obj(_class_instance), m_func(_method), m_p1(p1), m_p2(p2)
 	{
-	   _obj = _class_instance;
-	   _func = _method;
-	   _p1 = p1;
-	   _p2 = p2;
-	};
+	}
 
-	void operator()() { return (_obj->*_func)(_p1, _p2); }
-	void execute() { return operator()(); }
+	void operator()()   { return (m_obj->*m_func)(m_p1, m_p2); }
+	void execute()      { return operator()(); }
 
 private:
-
-	Class*  _obj;
-	Method  _func;
-	P1 _p1;
-	P2 _p2;
+	Class*  m_obj;
+	Method  m_func;
+	P1 m_p1;
+	P2 m_p2;
 };
 
 template <class Class, typename P1, typename P2, typename P3>
 class CallbackP3 : public CallbackBase
 {
+    typedef void (Class::*Method)(P1, P2, P3);
+    
 public:
-
-	typedef void (Class::*Method)(P1, P2, P3);
-	CallbackP3(Class* _class_instance, Method _method, P1 p1, P2 p2, P3 p3)
+	explicit CallbackP3(Class* _class_instance, Method _method, P1 p1, P2 p2, P3 p3) : m_obj(_class_instance), m_func(_method), m_p1(p1), m_p2(p2), m_p3(p3)
 	{
-	   _obj = _class_instance;
-	   _func = _method;
-	   _p1 = p1;
-	   _p2 = p2;
-	   _p3 = p3;
-	};
+	}
 
-	void operator()() { return (_obj->*_func)(_p1, _p2, _p3); }
-	void execute() { return operator()(); }
+	void operator()()   { return (m_obj->*m_func)(m_p1, m_p2, m_p3); }
+	void execute()      { return operator()(); }
 
 private:
-
-	Class*  _obj;
-	Method  _func;
-	P1 _p1;
-	P2 _p2;
-	P3 _p3;
+	Class*  m_obj;
+	Method  m_func;
+	P1 m_p1;
+	P2 m_p2;
+	P3 m_p3;
 };
 
 template <class Class, typename P1, typename P2, typename P3, typename P4>
 class CallbackP4 : public CallbackBase
 {
+    typedef void (Class::*Method)(P1, P2, P3, P4);
+    
 public:
-
-	typedef void (Class::*Method)(P1, P2, P3, P4);
-	CallbackP4(Class* _class_instance, Method _method, P1 p1, P2 p2, P3 p3, P4 p4)
+	explicit CallbackP4(Class* _class_instance, Method _method, P1 p1, P2 p2, P3 p3, P4 p4) : m_obj(_class_instance), m_func(_method), m_p1(p1), m_p2(p2), m_p3(p3), m_p4(p4)
 	{
-	   _obj = _class_instance;
-	   _func = _method;
-	   _p1 = p1;
-	   _p2 = p2;
-	   _p3 = p3;
-	   _p4 = p4;
-	};
+	}
 
-	void operator()() { return (_obj->*_func)(_p1, _p2, _p3, _p4); }
-	void execute() { return operator()(); }
+	void operator()()   { return (m_obj->*m_func)(m_p1, m_p2, m_p3, m_p4); }
+	void execute()      { return operator()(); }
 
 private:
-
-	Class*  _obj;
-	Method  _func;
-	P1 _p1;
-	P2 _p2;
-	P3 _p3;
-	P4 _p4;
+	Class*  m_obj;
+	Method  m_func;
+	P1 m_p1;
+	P2 m_p2;
+	P3 m_p3;
+	P4 m_p4;
 };
 
 #endif
