e0631452b323fc00a7cdcc193683abf44e6b735d
wayland <miroslav.kudrnac@hotmail.com>
2014-09-15 20:57:41 +0000

    + o

--- clib/cpuid.h
+++ clib/cpuid.h
@@ -17,25 +17,25 @@
 
 /* CPUID function */
 #ifdef WIN32
-void cpuid(int *CPUInfo, int InfoType)
-{
-    __cpuid(CPUInfo, InfoType);
-}
+    static inline void cpuid(int *CPUInfo, int InfoType)
+    {
+        __cpuid(CPUInfo, InfoType);
+    }
 #else
-void cpuid(int *CPUInfo, unsigned int number)
-{
-    /* Store number in %eax, call CPUID, save %eax, %ebx, %ecx, %edx in variables.
-     As output constraint "m=" has been used as this keeps gcc's optimizer
-     from overwriting %eax, %ebx, %ecx, %edx by accident */
-    __asm__("movl %4, %%eax; cpuid; movl %%eax, %0; movl %%ebx, %1; movl %%ecx, %2; movl %%edx, %3;"
-	: "=m" (CPUInfo[0]),
-        "=m" (CPUInfo[1]),
-        "=m" (CPUInfo[2]),
-        "=m" (CPUInfo[3])				  /* output */
-        : "r"  (number)                   /* input */
-        : "eax", "ebx", "ecx", "edx"      /* no changed registers except output registers */
-        );
-}
+    static inline void cpuid(int *CPUInfo, unsigned int number)
+    {
+        /* Store number in %eax, call CPUID, save %eax, %ebx, %ecx, %edx in variables.
+         As output constraint "m=" has been used as this keeps gcc's optimizer
+         from overwriting %eax, %ebx, %ecx, %edx by accident */
+        __asm__("movl %4, %%eax; cpuid; movl %%eax, %0; movl %%ebx, %1; movl %%ecx, %2; movl %%edx, %3;"
+        : "=m" (CPUInfo[0]),
+            "=m" (CPUInfo[1]),
+            "=m" (CPUInfo[2]),
+            "=m" (CPUInfo[3])				  /* output */
+            : "r"  (number)                   /* input */
+            : "eax", "ebx", "ecx", "edx"      /* no changed registers except output registers */
+            );
+    }
 #endif
 
 #endif
