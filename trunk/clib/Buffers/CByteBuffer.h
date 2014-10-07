71afa809ef20dd06a55082a091c44bd187b9b657
wayland <wayland@89b511ab-9c3f-d142-8dbd-1a3b8e1c1a46>
2014-10-07 15:29:48 +0000

    + CByteBuffer fix

--- trunk/clib/Buffers/CByteBuffer.h
+++ trunk/clib/Buffers/CByteBuffer.h
@@ -32,97 +32,103 @@ typedef struct CByteBuffer
 
 /** Create bytebuffer, storage is not allocated
  */
-#define bbuff_create(self)                                                              \
-    do                                                                                  \
-    {                                                                                   \
-        self = (bbuff*)_CALLOC(1, sizeof(bbuff));                                       \
+#define bbuff_create(self)                                                                  \
+    do                                                                                      \
+    {                                                                                       \
+        self = (bbuff*)_CALLOC(1, sizeof(bbuff));                                           \
     }while(0)
 
 /** Destroy bytebuffer and free storage
  */
-#define bbuff_destroy(self)                                                             \
-    do                                                                                  \
-    {                                                                                   \
-        _FREE((self)->storage);                                                         \
-        _FREE(self);                                                                    \
+#define bbuff_destroy(self)                                                                 \
+    do                                                                                      \
+    {                                                                                       \
+        _FREE((self)->storage);                                                             \
+        _FREE(self);                                                                        \
     }while(0)
     
     
 /** Reserve space in storage
  */
-#define bbuff_reserve(self, ressize)                                                    \
-    do                                                                                  \
-    {                                                                                   \
-        if(ressize > (self)->capacity)                                                  \
-        {                                                                               \
-            (self)->capacity = ressize;                                                 \
-            if(((self)->storage = (uint8*)_REALLOC((self)->storage, ressize)) == NULL)  \
-            {                                                                           \
-                assert(false);                                                          \
-            }                                                                           \
-        }                                                                               \
+#define bbuff_reserve(self, ressize)                                                        \
+    do                                                                                      \
+    {                                                                                       \
+        size_t _bb_ressize = (ressize);                                                     \
+        if(_bb_ressize > (self)->capacity)                                                  \
+        {                                                                                   \
+            (self)->capacity = _bb_ressize;                                                 \
+            if(((self)->storage = (uint8*)_REALLOC((self)->storage, _bb_ressize)) == NULL)  \
+            {                                                                               \
+                assert(false);                                                              \
+            }                                                                               \
+        }                                                                                   \
     }while(0)
     
 /** Resize storage
  */
-#define bbuff_resize(self, newsize)                                                     \
-    do                                                                                  \
-    {                                                                                   \
-        bbuff_reserve(self, newsize);                                                   \
-        (self)->size = newsize;                                                         \
-        (self)->rpos = 0;                                                               \
-        (self)->wpos = (self)->size;                                                    \
+#define bbuff_resize(self, newsize)                                                         \
+    do                                                                                      \
+    {                                                                                       \
+        size_t _bb_newsize = (newsize);                                                     \
+        bbuff_reserve(self, _bb_newsize);                                                   \
+        (self)->size = _bb_newsize;                                                         \
+        (self)->rpos = 0;                                                                   \
+        (self)->wpos = (self)->size;                                                        \
     }while(0)
 
 /** Append data to bytebuffer
  */
-#define bbuff_append(self, src, len)                                                    \
-    do                                                                                  \
-    {                                                                                   \
-        if(len > 0)                                                                     \
-        {                                                                               \
-            if((self)->size < ((self)->wpos + len))                                     \
-            {                                                                           \
-                bbuff_reserve(self, (self)->wpos + len);                                \
-                (self)->size = (self)->wpos + len;                                      \
-            }                                                                           \
-            memcpy((self)->storage + (self)->wpos, src, len);                           \
-            (self)->wpos += len;                                                        \
-        }                                                                               \
+#define bbuff_append(self, src, len)                                                        \
+    do                                                                                      \
+    {                                                                                       \
+        size_t _bb_len = (len);                                                             \
+        if(_bb_len > 0)                                                                     \
+        {                                                                                   \
+            if((self)->size < ((self)->wpos + _bb_len))                                     \
+            {                                                                               \
+                bbuff_reserve(self, (self)->wpos + _bb_len);                                \
+                (self)->size = (self)->wpos + _bb_len;                                      \
+            }                                                                               \
+            memcpy((self)->storage + (self)->wpos, src, _bb_len);                           \
+            (self)->wpos += _bb_len;                                                        \
+        }                                                                                   \
     }while(0)
    
 /** Read data from bytebuffer
  */
-#define bbuff_read(self, dst, len)                                                      \
-    do                                                                                  \
-    {                                                                                   \
-        if(((self)->rpos + len) <= (self)->size)                                        \
-        {                                                                               \
-            memcpy(dst, (self)->storage + (self)->rpos, len);                           \
-        }                                                                               \
-        else                                                                            \
-        {                                                                               \
-            memset(dst, 0, len);                                                        \
-        }                                                                               \
-        (self)->rpos += len;                                                            \
+#define bbuff_read(self, dst, len)                                                          \
+    do                                                                                      \
+    {                                                                                       \
+        size_t _bb_len = (len);                                                             \
+        if(((self)->rpos + _bb_len) <= (self)->size)                                        \
+        {                                                                                   \
+            memcpy(dst, (self)->storage + (self)->rpos, _bb_len);                           \
+        }                                                                                   \
+        else                                                                                \
+        {                                                                                   \
+            memset(dst, 0, _bb_len);                                                        \
+        }                                                                                   \
+        (self)->rpos += _bb_len;                                                            \
     }while(0)
     
 /** Put data to bytebuffer to specified position
  */
-#define bbuff_put(self, pos, src, len)                                                  \
-    do                                                                                  \
-    {                                                                                   \
-        assert(pos + len <= (self)->size);                                              \
-        memcpy((self)->storage + pos, src, len);                                        \
+#define bbuff_put(self, pos, src, len)                                                      \
+    do                                                                                      \
+    {                                                                                       \
+        size_t _bb_pos = (pos);                                                             \
+        size_t _bb_len = (len);                                                             \
+        assert(_bb_pos + _bb_len <= (self)->size);                                          \
+        memcpy((self)->storage + _bb_pos, src, _bb_len);                                    \
     }while(0)
     
 /** Clear bytebuffer data
  */
-#define bbuff_clear(self)                                                               \
-    do                                                                                  \
-    {                                                                                   \
-        _FREE((self)->storage);                                                         \
-        memset(self, 0, sizeof(bbuff));                                                 \
+#define bbuff_clear(self)                                                                   \
+    do                                                                                      \
+    {                                                                                       \
+        _FREE((self)->storage);                                                             \
+        memset(self, 0, sizeof(bbuff));                                                     \
     }while(0)
 
 #endif
